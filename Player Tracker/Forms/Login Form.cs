d30e7d71b2cccb40100e57cf4340d261ac235677
Dan Jesensky <frandstorage@gmail.com>
2014-04-29 14:04:18 -0400

    Initial commit

---
+++ Player Tracker/Forms/Login Form.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PlayerTracker.Forms {
+	public partial class frmLogin : Form {
+		public frmLogin() {
+			InitializeComponent();
+		}
+
+		private void btnLogin_Click(object sender, EventArgs e) {
+			Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			
+		}
+
+		private void btnExit_Click(object sender, EventArgs e) {
+			this.Close();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Player Tracker
+==============
+This program is to be written in C# (because our school's security software wasn't allowing it to connect to the Internet in Java). It is my capstone project for my senior year at Cuyahoga Valley Career Center (2013-2014).
+
+The application will include both a client- and server-side. The idea of the program is to help game and forum administrators keep track of players, their actions, wrongdoings, and the like. For data storage, a MySQL database will be used. Visual Studio's built-in unit testing framework will also be used for unit testing.
