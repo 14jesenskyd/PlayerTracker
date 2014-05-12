935ec332633ee74aabc2a57be077483f2926eb03
Dan Jesensky <frandstorage@gmail.com>
2014-05-12 18:52:35 -0400

    Fixed multiple server singleton instantiation.

--- Server/Server.cs
+++ Server/Server.cs
@@ -10,93 +10,94 @@ using System.Net;
 using System.Net.Sockets;
 
 namespace PlayerTracker.Server {
-	public class Server {
+    public class Server {
         public const string CONFIG_FILE = "server.cfg";
-		private static volatile Server singletonInstance = null;
-		private ConnectionManager connectionManager;
-		private DatabaseManager dbMan;
-		private Logger log;
-		private DataManager dataMan;
-		private Configuration config;
-		private bool accepting;
+        private static volatile Server singletonInstance = null;
+        private ConnectionManager connectionManager;
+        private DatabaseManager dbMan;
+        private Logger log;
+        private DataManager dataMan;
+        private Configuration config;
+        private bool accepting;
 
-		private Server()
-			: base() {
-			try {
-				this.log = new Logger("log.log");
-			} catch (IOException e) {
-				Console.WriteLine(e.Message);
-			}
+        private Server()
+            : base() {
+            Server.singletonInstance = this;
+            try {
+                this.log = new Logger("log.log");
+            } catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
             try {
                 this.config = Configuration.load(CONFIG_FILE);
                 this.connectionManager = new ConnectionManager(this.config.getValue<int>("port", 1534), this.config.getValue<string>("hostname", "127.0.0.1"));
                 this.connectionManager.start();
                 this.dbMan = new DatabaseManager(this.config.getValue<string>("db-hostname", "127.0.0.1"), this.config.getValue<int>("db-port", 3306), this.config.getValue<string>("db-user", "root"), this.config.getValue<string>("db-password", "root"), this.config.getValue<string>("db-database", "playertracker"));
                 this.dbMan.connect();
-				this.dataMan = new DataManager();
-				this.dataMan.start();
+                this.dataMan = new DataManager();
+                this.dataMan.start();
 
-				int rows = this.dbMan.executeNonQuery("SELECT * FROM information_schema.tables WHERE table_schema = 'playertracker' AND table_name = 'users' LIMIT 1;");
-				
-				
-				if (rows < 1) {
+                int rows = this.dbMan.executeNonQuery("SELECT * FROM information_schema.tables WHERE table_schema = 'playertracker' AND table_name = 'users' LIMIT 1;");
+
+
+                if (rows < 1) {
                     string sql = "";
                     StreamReader reader = new StreamReader("tables.sql");
                     while (reader.Peek() != -1)
                         sql += reader.ReadLine() + "\n";
-				    this.dbMan.executeNonQuery(sql);
-				}
-				this.accepting = true;
-			} catch (IOException e) {
-				this.log.error(e.Message);
-			}
-		}
+                    this.dbMan.executeNonQuery(sql);
+                }
+                this.accepting = true;
+            } catch (IOException e) {
+                this.log.error(e.Message);
+            }
+        }
 
-		public static Logger getLogger() {
-			return Server.getSingleton()._getLogger();
-		}
+        public static Logger getLogger() {
+            return Server.getSingleton()._getLogger();
+        }
 
-		public static Server getSingleton() {
-			if (Server.singletonInstance == null)
-				Server.singletonInstance = new Server();
-			return Server.singletonInstance;
-		}
+        public static Server getSingleton() {
+            if (Server.singletonInstance == null)
+                Server.singletonInstance = new Server();
+            return Server.singletonInstance;
+        }
 
-		public void toggleListening() {
-			if (this.accepting) {
-				this.connectionManager.stop();
-				this.dataMan.stop();
-				this.connectionManager.closeConnections();
-				this.accepting = false;
-			} else {
-				this.connectionManager.start();
-				this.dataMan.start();
-				this.accepting = true;
-			}
-		}
+        public void toggleListening() {
+            if (this.accepting) {
+                this.connectionManager.stop();
+                this.dataMan.stop();
+                this.connectionManager.closeConnections();
+                this.accepting = false;
+            } else {
+                this.connectionManager.start();
+                this.dataMan.start();
+                this.accepting = true;
+            }
+        }
 
-		public bool isAccepting() {
-			return this.accepting;
-		}
+        public bool isAccepting() {
+            return this.accepting;
+        }
 
-		private Logger _getLogger() {
-			return this.log;
-		}
+        private Logger _getLogger() {
+            return this.log;
+        }
 
-		public ConnectionManager getConnectionManager() {
-			return this.connectionManager;
-		}
+        public ConnectionManager getConnectionManager() {
+            return this.connectionManager;
+        }
 
-		public DatabaseManager getDbManager() {
-			return this.dbMan;
-		}
+        public DatabaseManager getDbManager() {
+            return this.dbMan;
+        }
 
-		public Dictionary<IPAddress, Connection> getConnections() {
-			return this.connectionManager.getConnections();
-		}
+        public Dictionary<IPAddress, Connection> getConnections() {
+            return this.connectionManager.getConnections();
+        }
 
-		public override string ToString() {
-			return "Server[" + this.getConnectionManager().ToString() + "]";
-		}
-	}
+        public override string ToString() {
+            return "Server[" + this.getConnectionManager().ToString() + "]";
+        }
+    }
 }
