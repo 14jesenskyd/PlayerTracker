e36825603bb7b74e79e7ec8e78e3a27f025c0c7b
Dan Jesensky <frandstorage@gmail.com>
2014-07-05 13:45:01 -0400

    Added tests for db

--- ServerTests/DatabaseTests.cs
+++ ServerTests/DatabaseTests.cs
@@ -4,14 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PlayerTracker.Server.Util;
+using PlayerTracker.Common.Util;
 
 namespace ServerTests {
 	[TestClass]
 	public class DatabaseTests {
 		[TestMethod]
 		public void TestConnection() {
-			DatabaseManager dbman = new DatabaseManager("127.0.0.1", 3306, "root", "root", "playertracker");
+			DatabaseManager dbman = new DatabaseManager("127.0.0.1", 3306, "root", "root", "playertracker-test");
 			dbman.connect();
+			dbman.Dispose();
+		}
+
+		[TestMethod]
+		public void TestTables() {
+			DatabaseManager dbman = new DatabaseManager("127.0.0.1", 3306, "root", "root", "playertracker-test", KVFactory.str("UserTable", "users"), KVFactory.str("ServerTable", "servers"), KVFactory.str("PlayerTable", "players"), KVFactory.str("AttachmentTable", "attachments"));
+			dbman.connect();
+
+			Assert.AreEqual<string>("users", dbman.getTable("UserTable"));
+			Assert.AreEqual<string>("servers", dbman.getTable("ServerTable"));
+			Assert.AreEqual<string>("attachments", dbman.getTable("AttachmentTable"));
+			Assert.AreEqual<string>("players", dbman.getTable("PlayerTable"));
+
+			dbman.Dispose();
 		}
 	}
 }
